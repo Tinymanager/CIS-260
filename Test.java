593d53838fc759e5b73d5c307c33d84334976110
Ethan Pedrick <ethan.pedrick@gmail.com>
2023-02-02 18:01:25 -0500

    c

--- Test.java
+++ Test.java
@@ -1,21 +1,20 @@
 public class Test {
     public static void main(String[] args) {
         int[][] field = new int[5][5];
-        int xpos = 3;
-        int ypos = 3;
+        int wxpos = 3;
+        int wypos = 3;
 
-        field[0][0]=0;        field[1][0]=1;        field[2][0]=0;        field[3][0]=2;        field[4][0]=0;
-        field[0][1]=0;        field[1][1]=0;        field[2][1]=0;        field[3][1]=0;        field[4][1]=0;
-        field[0][2]=1;        field[1][2]=0;        field[2][2]=4;        field[3][2]=0;        field[4][2]=0;
-        field[0][3]=0;        field[1][3]=0;        field[2][3]=0;        field[3][3]=0;        field[4][3]=0;
-        field[0][4]=3;        field[1][4]=3;        field[2][4]=3;        field[3][4]=3;        field[4][4]=3;
+        field[0][0]=1;        field[1][0]=11;        field[2][0]=12;        field[3][0]=2;        field[4][0]=0;
+        field[0][1]=2;        field[1][1]=21;        field[2][1]=0;        field[3][1]=0;        field[4][1]=0;
+        field[0][2]=3;        field[1][2]=31;        field[2][2]=4;        field[3][2]=0;        field[4][2]=0;
+        field[0][3]=4;        field[1][3]=41;        field[2][3]=0;        field[3][3]=0;        field[4][3]=0;
+        field[0][4]=5;        field[1][4]=51;        field[2][4]=3;        field[3][4]=3;        field[4][4]=3;
 
 
         int x = 0;
         int y = 0;
         int max =5;
         int xmax = 4;
-
         for (int ii=0; ii<max; ii++){
             System.out.print(field[x][y]);
             System.out.print(" ");
@@ -28,15 +27,5 @@ public class Test {
             y+=1;
             System.out.println(" ");
         }
-
-        /*for (int i=0; i<max; i++){
-            System.out.println(field[x][y]);
-            for (int ii=0; ii<max; ii++){
-                System.out.print(field[x]);
-                x+=1;
-            }
-            y=+1;
-            System.out.print(field[x][y]);
-        }*/
     }
 }

46aabf2960a9cd81e499b30d7ad7d987421372f3
Ethan Pedrick <ethan.pedrick@gmail.com>
2023-02-02 11:06:06 -0500

    c

--- Test.java
+++ Test.java
@@ -4,17 +4,39 @@ public class Test {
         int xpos = 3;
         int ypos = 3;
 
-        field[0][0]=0;
-        field[1][0]=1;
-        field[2][0]=0;
+        field[0][0]=0;        field[1][0]=1;        field[2][0]=0;        field[3][0]=2;        field[4][0]=0;
+        field[0][1]=0;        field[1][1]=0;        field[2][1]=0;        field[3][1]=0;        field[4][1]=0;
+        field[0][2]=1;        field[1][2]=0;        field[2][2]=4;        field[3][2]=0;        field[4][2]=0;
+        field[0][3]=0;        field[1][3]=0;        field[2][3]=0;        field[3][3]=0;        field[4][3]=0;
+        field[0][4]=3;        field[1][4]=3;        field[2][4]=3;        field[3][4]=3;        field[4][4]=3;
 
-        System.out.println(field[1][0]);
 
         int x = 0;
         int y = 0;
-        int max = 25;
-        for (int i=0; i<max; i++){
-            System.out.println(field[x][y]);
+        int max =5;
+        int xmax = 4;
+
+        for (int ii=0; ii<max; ii++){
+            System.out.print(field[x][y]);
+            System.out.print(" ");
+            for (int i=0; i<xmax; i++){
+                x+=1;
+                System.out.print(field[x][y]);
+                System.out.print(" ");
+            }
+            x=0;
+            y+=1;
+            System.out.println(" ");
         }
+
+        /*for (int i=0; i<max; i++){
+            System.out.println(field[x][y]);
+            for (int ii=0; ii<max; ii++){
+                System.out.print(field[x]);
+                x+=1;
+            }
+            y=+1;
+            System.out.print(field[x][y]);
+        }*/
     }
 }
